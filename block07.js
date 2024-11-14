c4e55d9da182cb7c45b8daf630d4fbf44e5c2255
James Barnes Jr <jamie@Jamies-MacBook-Air.local>
2024-11-14 12:26:03 -0500

    Adding files for the Vault project

---
+++ block07.js
@@ -0,0 +1,25 @@
+/*Pseudocode
+
+1. Create a string that tells the user You have received this message because you have been chosen to open an important vault. 
+Here is the secret combination:
+
+2. Create three different variables. Each variable will be the result of an arithmetic calculation using arithmetic operators.  
+The variables will equal 10 - 40 - 39.
+
+3. Create a dialog box using console.log to display the complet combiation which is produced by concatenating each individual variable. 
+
+*/
+
+
+// Create a welcome message.
+let welcomeMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+// Set three variables with the correct combination using arithmetic operators.
+let comboOne = 5 * 2;
+
+let comboTwo = 30 + 10;
+
+let comboThree = 45 - 6;
+
+console.log(welcomeMessage);
+console.log(comboOne + " - " + comboTwo + " - " + comboThree);
